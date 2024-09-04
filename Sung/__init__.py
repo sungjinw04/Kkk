972edaa07986de2cfdcdf4f13eda9e61901513a0
sungjinw04 <abhishekpandit4224@gmail.com>
2024-09-04 12:12:09 +0530

    Create __init__.py

---
+++ Sung/__init__.py
@@ -0,0 +1,19 @@
+import logging 
+from pyrogram import Client, idle
+
+from motor.motor_asyncio import AsynclOMotorClient
+from Sung.config.py import OWNER_ID, API_ID, API_HASH, MONGO_DB, SUPPORT_GROUP, SUPPORT_CHANNEL, BOT_TOKEN, BOT_USERNAME, JOINLOGS, LEAVELOGS
+
+class Config:
+  OWNER_ID = OWNER_ID
+  API_ID = API_ID
+  API_HASH = API_HASH
+  MONGO_DB = MONGO_DB
+  SUPPORT_GROUP = SUPPORT_GROUP
+  SUPPORT_CHANNEL = SUPPORT_CHANNEL
+  BOT_TOKEN = BOT_TOKEN
+  BOT_USERNAME = BOT_USERNAME
+  LEAVELOGS = LEAVELOGS
+  JOINLOGS = JOINLOGS
+  
+  
