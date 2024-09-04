a45d9000744e28c40c466a7c61468ec629f0cf86
sungjinw04 <abhishekpandit4224@gmail.com>
2024-09-04 14:26:03 +0530

    Update __main__.py

--- Sung/__main__.py
+++ Sung/__main__.py
@@ -1,1 +1,17 @@
+from Sung import logger 
+from pyrogram import idle
+import importlib
+from Sung import Bot
+from Sung.modules import ALL_MODULES
 
+for module_name in ALL_MODULES:
+    imported_module = importlib.import_module("Sung.modules." + module_name)
+
+if name == "main":
+  Bot.start()
+  logger.info("Bot is up and running...")
+  idle()
+  Bot.stop()
+  logger.info("Bot has stopped")
+
+    
